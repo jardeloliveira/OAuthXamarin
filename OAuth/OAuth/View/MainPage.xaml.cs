1af9f8a3e82679fab0ecefd4fa1b25c479c74b86
Jardel Oliveira <jardell.oliveira@hotmail.com>
2019-08-05 22:03:35 -0300

    OAuth

---
+++ OAuth/OAuth/View/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using OAuth.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OAuth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ OAuth/OAuth/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using OAuth.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace OAuth.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public ICommand LoginFacebookCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            LoginFacebookCommand = new Command(FacebookLogin);
+        }
+
+        private void FacebookLogin()
+        {
+            App.Current.MainPage = new LoginFacebook();
+        }
+    }
+}
